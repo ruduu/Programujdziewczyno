d27e8f31d5678508b3d5f4a6f37f03278de78c42
ruduu <32553769+ruduu@users.noreply.github.com>
2017-10-14 21:33:57 +0200

    zadanie 2

--- js/app.js
+++ js/app.js
@@ -1,20 +1,60 @@
     'use strict';
 
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
+   
+    var ForDropdown = document.querySelector('.for-dropdown');
+    var Dropdown = document.querySelector ('.dropdown');
 
-        var ForDropdown = document.querySelector('.for-dropdown');
-        var Dropdown = document.querySelector('.dropdown');
+    function otworz() {
+    Dropdown.style.display = 'block';
+    }
+    
+    function zamknij() {
+    Dropdown.style.display = 'none';
+    }
+    
+    ForDropdown.addEventListener('mouseover', otworz);
+    ForDropdown.addEventListener('mouseout', zamknij);
+  
+    
+    
+    
+    var buttons = document.querySelectorAll('.read-more');
 
-        function otworz() {
-        Dropdown.style.display = 'block';
-        }
-       
-        function zamknij() {
-        Dropdown.style.display = 'none';    
-        }
-        
-        ForDropdown.addEventListener ('mouseover', otworz); 
-        ForDropdown.addEventListener ('mouseout', zamknij); 
-        
+    
+//buttons[0].addEventListener('click', function() {
+//      var textArea = this.previousElementSibling;
+//      textArea.style.display = 'block';
+//      
+//     console.log(this.innerHTML); 
+//    });
+//  
+//        buttons[0].addEventListener('click', function() {
+//      var textArea = this.previousElementSibling;
+//      textArea.style.display = 'block';
+//      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+//    });
+    
+    
 
-    });
+    
+    
+     function showHide() {
+      var textArea = this.previousElementSibling;
+    
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+    
+     }
+    
+    
+          for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+          }
+    
+});
