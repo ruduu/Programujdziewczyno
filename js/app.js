e9b0715f7bc38a3512cb97619f67e8400c23de02
ruduu <anna.sameryt@gmail.com>
2017-12-17 19:40:54 +0100

    zadanie 1 17 grudnia

--- js/app.js
+++ js/app.js
@@ -1,69 +1,42 @@
-    'use strict';
-
-document.addEventListener('DOMContentLoaded', function(){
-   
-    var ForDropdown = document.querySelector('.for-dropdown');
-    var Dropdown = document.querySelector ('.dropdown');
-
-    function otworz() {
+'use strict';  
+console.log(menu.style);
+document.addEventListener('DOMContentLoaded', function() {
+      
+  var Dropdown = document.querySelector('.dropdown');           
+  var ForDropdown = document.querySelector('.for-dropdown');         
+               
+      function otworz() {
     Dropdown.style.display = 'block';
     }
     
     function zamknij() {
     Dropdown.style.display = 'none';
+        console.log('czy dziala');
     }
     
     ForDropdown.addEventListener('mouseover', otworz);
     ForDropdown.addEventListener('mouseout', zamknij);
-  
-    
-    
-    
-    var buttons = document.querySelectorAll('.read-more');
-
-    
-//buttons[0].addEventListener('click', function() {
-//      var textArea = this.previousElementSibling;
-//      textArea.style.display = 'block';
-//      
-//     console.log(this.innerHTML); 
-//    });
-//  
-//        buttons[0].addEventListener('click', function() {
-//      var textArea = this.previousElementSibling;
-//      textArea.style.display = 'block';
-//      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-//    });
-    
-    
-
-    
-    
-     function showHide() {
-      var textArea = this.previousElementSibling;
-    
-      if (textArea.style.display === 'none' || textArea.style.display === '') {
-        textArea.style.display = 'block';
-        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-      } else {
-        textArea.style.display = 'none';
-        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
-      }
-    
-     }
-    
-    
-          for (var i = 0; i <= buttons.length-1; i++) {
-        buttons[i].addEventListener('click', showHide);
-          }
-    
-    var menu = document.querySelector('.navbar');
-    
-    window.addEventListener('scroll', function() {
-        menu.style.borderBottom = '1px solid #7f7f7f';
-        console.log (menu);
+          
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     });
-    
-
-    
-});
