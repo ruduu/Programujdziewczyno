3386d3980d43fd378e04d2f7323a08d22d21a5a6
ruduu <anna.sameryt@gmail.com>
2017-12-17 21:31:16 +0100

    zadanie 2 17 grudnia

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict';  
-console.log(menu.style);
+
 document.addEventListener('DOMContentLoaded', function() {
-      
-  var Dropdown = document.querySelector('.dropdown');           
-  var ForDropdown = document.querySelector('.for-dropdown');         
+    
+    
+// 1 zadanie 
+    
+    var Dropdown = document.querySelector('.dropdown');           
+    var ForDropdown = document.querySelector('.for-dropdown');         
                
       function otworz() {
     Dropdown.style.display = 'block';
@@ -16,20 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
     
     ForDropdown.addEventListener('mouseover', otworz);
     ForDropdown.addEventListener('mouseout', zamknij);
-          
+      
+ // 2 zadanie   
+    
+  var buttons = document.querySelectorAll('.read-more');
+
+  function showHide() {
+
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+// 3 zadanie 
         
         
         
