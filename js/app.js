5ed9f4ca1cba1bfb66d531742d3d50999af9632f
ruduu <anna.sameryt@gmail.com>
2017-12-19 20:42:12 +0100

    zaanie 6 19 grudnia

--- js/app.js
+++ js/app.js
@@ -63,6 +63,58 @@ document.addEventListener('DOMContentLoaded', function() {
   backToTopBtn.addEventListener('click', function() {
     window.scroll( 0, 0 );
   });
+    
+    
+// 5 zadanie
+    
+    var items = document.querySelectorAll('.slider li');
+    var dots = document.querySelectorAll('.dot');
+    
+    var dot1 = document.getElementById('dot1');
+    var dot2 = document.getElementById('dot2');                                   
+    var dot3 = document.getElementById('dot3');
+    
+    dot1.addEventListener('click', function() {
+         for (var i = 0; i < items.length; i++) {
+            dots[i].classList.remove('active');
+            items[i].classList.remove('visible');
+    };
+        
+      this.classList.add('.active');
+      items[0].classList.add('visible');
+      });
+    
+    dot2.addEventListener('click', function() {
+    for (var i = 0; i < items.length; i++) {
+      items[i].classList.remove('visible');
+      dots[i].classList.remove('active');
+    };
+    this.classList.add('active');
+    items[1].classList.add('visible');
+});
+
+    dot3.addEventListener('click', function() {
+    for (var i = 0; i < items.length; i++) {
+      items[i].classList.remove('visible');
+      dots[i].classList.remove('active');
+    };
+    this.classList.add('active');
+    items[2].classList.add('visible');
+  });
+
+    
+  // zadanie 6  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //to musi zostac
  });
     
 
